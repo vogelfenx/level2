889ebe1531e3220240a94438023c60ac97777b1c
Kirill Fedyakov <KFedyakov@ee.lan>
2019-09-16 23:54:41 +0200

    create form

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 /* ui */
 import "./ui/button/button.scss"
 import "./ui/button/button_color--blue.scss"
+import "./ui/text/text.scss"
 
 /* import blocks */
 import "$blocks/nav/nav.scss";
 import "$blocks/nav/__item/nav__item.scss";
 import "$blocks/nav/__button/nav__button.scss";
+import "$blocks/form/form.scss"
 
-/* import componets (mixins) */
+/* import mixins */
 import "$components/header/header.scss";
 import "$components/header/__logo/header__logo.scss";
 import "$components/header/__nav/header__nav.scss";
